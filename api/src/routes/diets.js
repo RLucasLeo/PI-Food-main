d3fbdae75dc9f05f355a9483d65a1f419cd075c9
lucas <robledolucas42@gmail.com>
2023-06-02 11:50:23 -0300

    regresion back diets

--- api/src/routes/diets.js
+++ api/src/routes/diets.js
@@ -1,16 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 const {Diets} = require('../db');
-const {allDiets, DietsInDb} = require('../controllers/diets')
+const {allDiets} = require('../controllers/diets')
 const { Op } = require('sequelize');
 
-router.get('/', async (req,res)=>{
-    try {
-        const diets = await DietsInDb()
-        res.status(200).json(diets)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
+router.get('/', allDiets)
 
 module.exports= router;
