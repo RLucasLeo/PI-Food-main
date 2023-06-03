22abf5ca37dd5d80e9116ba29cf3c26e81da25ad
lucas <robledolucas42@gmail.com>
2023-06-02 22:16:28 -0300

    imagen error en proceso

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -3,12 +3,12 @@ import React from "react"
 import {Link} from "react-router-dom"
 
 const Card = (props)=> {
-
+    const imgCarga = <img className={s.image} src="https://img.freepik.com/fotos-premium/manos-femeninas-sostienen-cubiertos-sobre-plato-vacio-naranja_185193-33404.jpg?w=740" alt="Imagen no disponible" />
        return (
            <div className={s.card}>
                
                    <h3 className={s.nombre}>{props.title}</h3>
-                   <img className={s.image} src={props.image} alt=""/>
+                   {props.image? (<img className={s.image} src={props.image} alt=""/>) : imgCarga }
                    <p className={s.genres}>{props.diets}</p>                  
                 <Link to={`/detail/${props.id}`} className={s.navLink}><span className={s.leer_mas}>Leer más</span></Link>
             
