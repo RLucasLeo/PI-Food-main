0fcd79e3e8f809435f5ddad2082ea7fa6b925ac8
lucas <robledolucas42@gmail.com>
2023-06-02 21:49:47 -0300

    Boton borrar receta hecho

--- client/src/Components/Detail/Detail.jsx
+++ client/src/Components/Detail/Detail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
-import { getRecipesById } from "../../redux/actions/index";
+import { getRecipesById, deleteRecipes } from "../../redux/actions/index";
 import s from './Detail.module.css'
 
 
@@ -17,7 +17,7 @@ export default function Detail() {
     }, [dispatch, id])
 
     const details = useSelector(state => state.details)
-    console.log(details)
+    console.log("Log del details",details)
 
     if (carga) {
         return "cargando paaa";
@@ -25,26 +25,41 @@ export default function Detail() {
 
     var regex = /(<([^>]+)>)/gi;
 
-    const { title, image, healthScore, diets, summary } = details[0];
+    const { title, image, healthScore, diets, summary, createdAt } = details[0];
 
     return (
         <div className={s.card}>
-            <h4>ID de la receta: {id}</h4><h1>{title}</h1> 
+           
+            <h4>ID de la receta: {id}</h4>
+           
+            <h1>{title}</h1> 
+            
             <img className={s.image}src={image} alt={`${title}'s`}/>
             <br />
+           
             <div className={s.texto}>
                  <div >💡Summary: {summary?.replace(regex, '').replace('&#39', '')}</div>
             </div>
+
             <p >💪HealthScore: {healthScore}</p>
+            
             <div>
         <h3>📋Step by step:</h3>
         <div>
           {Array.isArray(details[0].instructions) ? details[0].instructions.map(e => e.steps.map(f => (
           <li>{f.step}</li>))) : details[0].instructions.split('|').map(step => <li key={step}>{step}</li> ) }
         </div>
+        
       </div>
+
             <p>❤️Type of diets: {diets?.map(g => (g.name ? g.name : g)).join(' | ')}</p>
-                
+           
+            {createdAt ? (
+                <button onClick={() => dispatch(deleteRecipes(id))}>
+                  Delete
+                </button>
+              ) : null}
+
             <div >
                 <div>
                     <NavLink to={'/home'} className={s.myButton}>
@@ -52,6 +67,7 @@ export default function Detail() {
                     </NavLink>
                 </div>
             </div>
+
         </div>
     );
 }
