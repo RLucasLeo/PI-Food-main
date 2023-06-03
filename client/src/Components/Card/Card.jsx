c815c7d17ef737725c745a9e89d0a07a1bb174b5
lucas <robledolucas42@gmail.com>
2023-06-02 21:49:07 -0300

    componente render a function

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,21 +1,24 @@
 import s from "./Card.module.css"
 import React from "react"
 import {Link} from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { deleteRecipes } from "../../redux/actions"
 
-class Card extends React.Component {
+const Card = (props)=> {
 
-   render() {
+   const dispatch = useDispatch()
        return (
            <div className={s.card}>
                
-                   <h3 className={s.nombre}>{this.props.title}</h3>
-                   <img className={s.image} src={this.props.image} alt=""/>
-                   <p className={s.genres}>{this.props.diets}</p>
-                <Link to={`/detail/${this.props.id}`} className={s.navLink}><span className={s.leer_mas}>Leer más</span></Link>
+                   <h3 className={s.nombre}>{props.title}</h3>
+                   <img className={s.image} src={props.image} alt=""/>
+                   <p className={s.genres}>{props.diets}</p>                  
+                <Link to={`/detail/${props.id}`} className={s.navLink}><span className={s.leer_mas}>Leer más</span></Link>
             
            </div>
        )
-   }
+   
 
 }
+
 export default Card;
